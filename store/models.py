0b94ad33094127c5537840473e37f875fe93f96a
akashSharma135 <akashsharma1352000@gmail.com>
2022-04-08 23:24:49 +0530

    created Product model

---
+++ store/models.py
@@ -0,0 +1,18 @@
+from category.models import Category
+from django.db import models
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    description = models.TextField(max_length=500, blank=True)
+    price = models.PositiveIntegerField()
+    images = models.ImageField(upload_to='images/products')
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.product_name
