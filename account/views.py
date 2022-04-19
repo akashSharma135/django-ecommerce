fe63df389d95791185d58f8bd5f9a3216970bb28
akashSharma135 <akashsharma1352000@gmail.com>
2022-04-19 22:41:44 +0530

    password reset functionality done

--- account/views.py
+++ account/views.py
@@ -76,7 +76,6 @@ def login(request):
 @login_required(login_url = 'login')
 def logout(request):
     auth.logout(request=request)
-    messages.success("You are logged out")
     return redirect(reverse('login'))
 
 
@@ -95,3 +94,70 @@ def activate(request, uidb64, token):
         user.save()
         messages.success(request=request, message="Congratulations! Your account has been activated.")
         return redirect('login')
+
+def reset_password_validate(request, uidb64, token):
+    try:
+        # decode user id
+        uid = urlsafe_base64_decode(uidb64).decode()
+        # get the user
+        user = Account._default_manager.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid'] = uid
+        messages.success(request=request, message="Please reset your password")
+        return redirect('reset_password')
+    else:
+        messages.error(request=request, message="This link has been expired!")
+        return redirect('login')
+
+
+def reset_password(request):
+    if request.method == 'POST':
+        data = request.POST
+        new_password = data['new_password']
+        confirm_password = data['confirm_password']
+
+        if new_password != confirm_password:
+            messages.error(request=request, message="New Password and Confirm Password didn't match.")
+
+        uid = request.session['uid']
+        user = Account.objects.get(pk=uid)
+        user.set_password(new_password)
+        user.save()
+        messages.success(request=request, message="Password reset successful!")
+        return redirect('login')
+
+    return render(request=request, template_name='account/reset_password.html')
+
+
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request=request, template_name='account/dashboard.html')
+
+
+def forgot_password(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        try:
+            user = Account.objects.get(email__exact=email)
+            current_site = get_current_site(request)
+            mail_subject = "Reset your Password"
+            message = render_to_string("account/reset_password_template.html", {
+                "user": user,
+                "domain": current_site,
+                "uid": urlsafe_base64_encode(force_bytes(user.id)),
+                "token": default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, settings.EMAIL_HOST_USER, to=[to_email]) 
+            send_email.send()
+
+            messages.success(request=request, message="We have sent you a mail at " + email)
+            return redirect(to='login')
+
+        except Account.DoesNotExist:
+            messages.success(request=request, message="Account Does not exists!")
+            return redirect(to='register')
+    return render(request=request, template_name='account/forgot-password.html')
